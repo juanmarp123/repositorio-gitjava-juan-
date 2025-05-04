e1733f209e878baf243a1f2c0a66c5aeaa40720b
Juan Martinez <Juan Martinez@LAPTOP-LBLG9RN7>
2025-05-04 11:19:27 -0500

    CREE EL PRIMER FORMULARIO CON 2 BOTONES

---
+++ practicadeusoGIT/src/main/java/com/mycompany/practicadeusogit/PracticadeusoGIT.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.practicadeusogit;
+
+/**
+ *
+ * @author Juan Martinez
+ */
+public class PracticadeusoGIT {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
